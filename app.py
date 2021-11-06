78b3fd7cfcb1c8b5253c67355f4e1ba4d3ec8725
Jackfrank <vincent1990wen@gmail.com>
2021-11-06 22:02:10 +0800

    完成通信

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request
+from flask_cors import CORS
+
 
 app = Flask(__name__,
             static_folder = 'web/static',
@@ -14,4 +16,20 @@ def index():
 def flow():
      return render_template('transaction.html')
 
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        print(request.files['file'])
+        f = request.files['file']
+        # data_xls = pd.read_excel(f)
+        # return data_xls.to_html()
+@app.route("/me", methods=['POST'])
+def me_api():
+    return {
+        "username": "username",
+        "theme": "username",
+        "image": "username",
+    }
+
 app.run(debug=True)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
