17188caf31a82c91083d4e9099527139a2ab69ac
Jackfrank <vincent1990wen@gmail.com>
2021-11-06 15:43:23 +0800

    完成列表

--- app.py
+++ app.py
@@ -3,8 +3,15 @@ from flask import Flask,render_template
 app = Flask(__name__,
             static_folder = 'web/static',
             template_folder = 'web/templates'
-)
+    )
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route('/transaction')
+def flow():
+     return render_template('transaction.html')
+
+app.run(debug=True)
--- sql/transaction.sql
+++ sql/transaction.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `transaction`(
     description VARCHAR(200) COMMENT '描述',
     account_type INT COMMENT '消费账户 1.老公钱包 2.老婆钱包 ',
     payment_type INT COMMENT '付款类型 1.支付宝 2.微信 3.银行卡 4.现金',
-    member INT COMMENT '消费对象 1.家庭 2.老公 3.老婆 4.牧 ',
+    customer INT COMMENT '消费对象 1.家庭 2.老公 3.老婆 4.牧 ',
     create_time DATETIME COMMENT'创建时间',
     tag INT COMMENT '标签 1.日常消费（买菜，沃尔玛） 2.一次性消费（零食，购物衣服） 3.固定消费（水电煤，油费，理发话费）'
 
