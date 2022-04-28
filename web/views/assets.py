1558b1893af31ada2012594c07cda26a5ffbe5e9
JackFrankWen <vincent1990wen@gmail.com>
2022-04-28 13:23:13 +0800

    feat:创建

--- web/views/assets.py
+++ web/views/assets.py
@@ -1,8 +1,7 @@
 """d"""
 from flask import Blueprint, render_template, request
-from web.api.my_con import run_mysql
+from web.api.my_con import run_mysql,query_mysql
 from web.enum.enum import to_dict_consumer,to_dict_risk_rank,to_dict_account_type
-import json
 
 assets_blueprint = Blueprint('assets', __name__ ,
                            static_folder='web/static',
@@ -10,21 +9,83 @@ assets_blueprint = Blueprint('assets', __name__ ,
                            template_folder='/web/templates')
 
 
+
 @assets_blueprint.route("/assets/cate/insert", methods=['POST'])
 def assets_cate_insert():
     """
-    获取接口
+    资产大类
     """
     try:
-        data = request.get_data()
-        data = json.loads(data)
+        data = request.get_json(force=True)
         if data.get('id'):
             assets_cate_update(data)
         else:
             assets_cate_create(data)
-    except  BaseException as err:
+    except BaseException as err:
         print(err)
-    return {'data': 200}
+    return {'code': 200}
+
+
+@assets_blueprint.route("/assets/insert", methods=['POST'])
+def assets_insert():
+    """
+    资产条目
+    """
+    try:
+        data = request.get_json(force=True)
+
+        if data.get('id'):
+            assets_update(data)
+        else:
+            assets_create(data)
+    except BaseException as err:
+        print(err)
+    return {'code': 200}
+
+
+def assets_create(data):
+    """
+    表assets创建
+    :param data:
+    """
+    query_clause = f"""
+       INSERT INTO assets
+            (amount, assets_cate_id, record_time)
+        VALUES ({data.get('amount')},
+            "{data.get('assets_cate_id')}",
+            "{data.get('record_time')}")
+           """
+    print(query_clause)
+    run_mysql(query_clause, "")
+
+
+@assets_blueprint.route("/assets/cate/get", methods=['POST'])
+def get_assets_cate():
+    data = request.get_json(force=True)
+
+    """
+    获取接口
+    """
+
+    query = f"""SELECT * FROM assets_cate
+    where account_type = {data.get('account_type')}
+    """
+    data = query_mysql(query, '')
+    return {'data': data, 'code': 200}
+
+
+@assets_blueprint.route("/assets/get", methods=['POST'])
+def get_assets():
+    """
+    获取接口
+    """
+    data = request.get_json(force=True)
+
+    query = f"""SELECT * FROM assets_cate 
+    where account_type = {data.get('account_type')}
+    """
+    data = query_mysql(query, '')
+    return {'data': data, 'code': 200}
 
 
 @assets_blueprint.route("/assets/enum", methods=['POST'])
@@ -41,9 +102,10 @@ def assets_enum():
         'risk_rank': risk_rank,
         'account_type': account_type,
     }
-    return {'code': 200,
-            'data': dict_data
-            }
+    return {
+        'code': 200,
+        'data': dict_data
+    }
 
 
 def assets_cate_create(data):
@@ -51,20 +113,34 @@ def assets_cate_create(data):
     query_clause = f"""
     INSERT INTO assets_cate
          (description, category, account_type, risk_rank)
-         VALUES ("{data.get('description')}", "{data.get('category')}",  {data.get('account_type')}, {data.get('risk_rank')})
+    VALUES ("{data.get('description')}",
+        "{data.get('category')}",
+        {data.get('account_type')},
+        {data.get('risk_rank')})
         """
-    print(query_clause)
     run_mysql(query_clause, "")
 
 
 def assets_cate_update(data):
-    """更新"""
+    """ assets_cate更新
+    """
     query_clause = f"""
     UPDATE assets_cate
     SET description="{data.get('description')}",
     category="{data.get('category')}",
     account_type={data.get('account_type')},
     risk_rank={data.get('risk_rank')}
+    WHERE id = {data.get('id')}
+    """
+    run_mysql(query_clause, "")
+
+
+def assets_update(data):
+    """更新 assets表更新"""
+    query_clause = f"""
+    UPDATE assets
+    SET amount="{data.get('amount')}",
+    record_time="{data.get('record_time')}"
     WHERE id = {data.get('id')}
     """
     run_mysql(query_clause, "")
