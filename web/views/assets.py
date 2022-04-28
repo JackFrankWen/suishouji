faaf761b601c67e13c0774045cd3ef034e8fb89d
JackFrankWen <vincent1990wen@gmail.com>
2022-04-28 13:54:44 +0800

    feat:创建成功

--- web/views/assets.py
+++ web/views/assets.py
@@ -43,6 +43,44 @@ def assets_insert():
     return {'code': 200}
 
 
+@assets_blueprint.route("/assets/insert/batch", methods=['POST'])
+def assets_insert_batch():
+    """
+    资产条目
+    """
+    try:
+        data = request.get_json(force=True)
+
+        assets_create_batch(data)
+    except BaseException as err:
+        print(err)
+    return {'code': 200}
+
+
+def assets_create_batch(data):
+    """
+    表assets创建
+    :param data:
+    """
+    values = f""""""
+    for key, value in enumerate(data):
+        if key == len(data) - 1:
+            values += f"""
+             ({value.get('amount')},"{value.get('assets_cate_id')}","{value.get('record_time')}")
+             """
+        else:
+            values += f"""
+                    ({value.get('amount')},"{value.get('assets_cate_id')}","{value.get('record_time')}"),
+                    """
+
+    query_clause = f"""
+       INSERT INTO assets
+            (amount, assets_cate_id, record_time)
+        VALUES {values}
+           """
+    run_mysql(query_clause, "")
+
+
 def assets_create(data):
     """
     表assets创建
